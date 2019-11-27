01d0ed2e485da83506e3c16e78900c72ac53ff39
Kendra Wu <40707182+kendrawu@users.noreply.github.com>
2019-11-27 11:20:35 +0000

    Add function func_InfectSize
    Vary the max number of infected individuals at each timestep according to R0 and the number of infectious individuals at previous timestep

--- sbm1.jl
+++ sbm1.jl
@@ -26,24 +26,25 @@ N = 500 #Total population size
 endtime = 100.0 #Duration of simulation (in days)
 par_ode = [0.94,0.19,0.6,27.79,0.14,0.33] #Parameter values of ODE system for beta
 tspan = (0.0,endtime)
-case0num = 10 #Number of infectious individuals introduced into the community to begin the outbreak
+
 ## The clustered network
-communitynum = 1 #Number of communities in the clustered network
-communitysize_avg = 100 #Average size of one clustered community
-communitysize_range = 40 #Range of community sizes (sizes are uniformly distributed)
-rates_within = 0.15 #Probability of an edge between two nodes in the same community
-rates_between = 0.001 #Probability of an edge between two nodes in different communities
+#communitynum: Number of communities in the clustered network
+#communitysize_avg: Average size of one clustered community
+#communitysize_range: Range of community sizes (sizes are uniformly distributed)
+#rates_within: Probability of an edge between two nodes in the same community
+#rates_between: Probability of an edge between two nodes in different communities
+par_cluster = DataFrame(communitynum=1, communitysize_avg=100, communitysize_range=40, rates_within=0.15, rates_between=0.001)
+
 ## Disease properties - Gamma-distributed
-incubperiod_shape = 3.11
-incubperiod_rate = 0.32
-infectperiod_shape = 1.13
-infectperiod_rate = 0.226
+casenum0 = 10 #Number of infectious individuals introduced into the community to begin the outbreak
+lambda = 0.01 #Per-time-step harzard of infection for a sucsptible nodes from an infectious neighbour
+# Use Ebola-like parameters (from Hitchings (2018))
+par_disease = DataFrame(incubperiod_shape=3.11, incubperiod_rate=0.32, infectperiod_shape=1.13, infectperiod_rate=0.226)
 incubperiod_avg = ceil(incubperiod_shape/incubperiod_rate)
 infectperiod_avg = ceil(infectperiod_shape/infectperiod_rate)
 
 # Initializations
 beta_t = ones(round(Int,endtime+1)) #Initialize beta_t
-InfectSize = case0num #Initialize the maximum number of infected individuals at each timestep
 sbm = hcat(DataFrame(nodes_name=1:N, nodes_status=Vector{Union{Missing, String}}(missing, N), e_nodes_infectday=zeros(N), e_nodes_incubperiod=zeros(N), i_nodes_infectperiod=zeros(N)), makeunique=true)
 sbm.nodes_status = "S" #Set initial status of each node as susceptible
 nstatus = convert(DataFrame,Array{Union{Missing, String}}(missing, N, round(Int,endtime+1)))
@@ -51,7 +52,8 @@ for r1=1:N, r2=1:round(Int,endtime)
     nstatus[r1,r2] = "S"
 end
 nstatus = insertcols!(nstatus, 1, nodes_name=1:N)
-sbm_sol = DataFrame(S=fill(0,round(Int,endtime+1)), E=fill(0,round(Int,endtime+1)), I=fill(0,round(Int,endtime+1)), R=fill(0,round(Int,endtime+1)), N=fill(0,round(Int,endtime+1))) #Initialize the matrix which holds SEIR incidence of all timestep
+sbm_sol = DataFrame(S=fill(0,round(Int,endtime)), E=fill(0,round(Int,endtime)), I=fill(0,round(Int,endtime)), R=fill(0,round(Int,endtime)), N=fill(0,round(Int,endtime))) #Initialize the matrix which holds SEIR incidence of all timestep
+
 
 # Function to return rate of transmission, beta
 function func_beta(beta,p,t)
@@ -59,6 +61,22 @@ function func_beta(beta,p,t)
         # p[1]=betahat; p[2]=a1; p[3]=a2; p[4]=atau; p[5]=sigma; p[6]=gamma
         beta[t] = p[1] * (1 - p[3]/(1 + exp(-p[2] * (t - p[4]))))
     end
+end
+
+#Function to return the maximum number of infected individuals at each timestep
+#according to R0 and number of infectious individuals at previous timestep
+function func_InfectSize(sbm_sol, par_cluster, timestep)
+    # R0 computation based on equation from Hitchings (2018)
+    R0 = (1 - (par_disease[1,:infectperiod_rate]/(par_disease[1,:infectperiod_rate]+lambda))^par_disease[1,:infectperiod_shape]) * (((par_cluster[1,:communitysize_avg]-1)*(1-par_cluster[1,:rates_within])*par_cluster[1,:rates_within] + (par_cluster[1,:communitynum]-1)*par_cluster[1,:communitysize_avg]*(1-par_cluster[1,:rates_between])*par_cluster[1,:rates_between] + ((par_cluster[1,:communitysize_avg]-1)*par_cluster[1,:rates_within] + (par_cluster[1,:communitynum]-1)*par_cluster[1,:communitysize_avg]*par_cluster[1,:rates_between])^2)/ ((par_cluster[1,:communitysize_avg]-1)*par_cluster[1,:rates_within] + (par_cluster[1,:communitynum]-1)*par_cluster[1,:communitysize_avg]*par_cluster[1,:rates_between])-1)
+
+    #Compute the maximum number of infected individuals at each timestep
+    if timestep == 1
+        InfectSize = casenum0
+    else
+        InfectSize = R0 * sbm_sol.I[timestep-1]
+        InfectSize = round(Int, InfectSize)
+    end
+    return InfectSize
 end
 
 # Function to construct the who-infect-whom network and return it as a DataFrame
@@ -69,9 +87,9 @@ function func_network(N)
 
     # Format the who-infect-whom network into a DataFrame
     savegraph("./data/sbm.csv", g) #Write results into a file
-    #For PC
+    #For my PC
     x = CSV.read(joinpath(dirname(pathof(DataFrames)), "C:/Users/kendraw/OneDrive - Nexus365/ADAGIO/Scripts/data/sbm.csv")) #Convert g into DataFrame and name it as x
-    #For Mac                                           
+    #For my Mac
     #x = CSV.read(joinpath(dirname(pathof(DataFrames)), "/Users/kendramwu/OneDrive - Nexus365/ADAGIO/Scripts/data/sbm.csv")) #Convert g into DataFrame and name it as x
     x = select!(x, Not(3:7)) #Delete the junk columns
     x = DataFrame(Infector=x[:,1], Infectee=x[:,2]) #Add column names to x
@@ -79,7 +97,6 @@ function func_network(N)
     #Find the unique values in x, so that no node is infected twice
     y = unique(x.Infectee)
 
-    #G = dummy[1:InfectSize] #Take the first InfectSize number from dummy
     return y
 end
 
@@ -153,60 +170,66 @@ function func_countelements(v)
     return D
 end
 
-# Solve the ODE system
-#func_beta(beta,par_ode,tspan)
 
 # Compute the parameters for disease properties
-infectperiod = Gamma(infectperiod_shape,infectperiod_rate) #Infectious period of the disease
-incubperiod = Gamma(incubperiod_shape,incubperiod_rate) #Incubation period of the disease
+incubperiod = Gamma(par_disease[1,:incubperiod_shape],par_disease[1,:incubperiod_rate]) #Incubation period of the disease
+infectperiod = Gamma(par_disease[1,:infectperiod_shape],par_disease[1,:infectperiod_rate]) #Infectious period of the disease
 
 # Compute the parameters for the clusters and network
-communitysize_avg_mat = [(communitysize_avg) for i=1:communitynum]
-random_mat = rand(Uniform(-communitysize_range/2,communitysize_range/2), (communitynum, communitynum)) #Obtain a random community size value based on uniform distribution
+communitysize_avg_mat = [(par_cluster[1,:communitysize_avg]) for i=1:par_cluster[1,:communitynum]]
+random_mat = rand(Uniform(-par_cluster[1,:communitysize_range]/2,par_cluster[1,:communitysize_range]/2), (par_cluster[1,:communitynum], par_cluster[1,:communitynum])) #Obtain a random community size value based on uniform distribution
 random_mat = round.(Int, random_mat)
 communitysize = communitysize_avg_mat + random_mat
 studypopsize = sum(communitysize)
-rates_mat = [rates_within + rates_between]
+rates_mat = [par_cluster[1,:rates_within] + par_cluster[1,:rates_between]]
 #Cluster = zeros(studypopsize) #Form an arry of the cluster with population size studypopsize
+
 
 # Main algorithm
 for timestep1 in 1:(round(Int,endtime))
     y = func_network(N) #Construct the who-infect-whom network
+    InfectSize = func_InfectSize(sbm_sol, par_cluster, timestep1) #Maximum number of infected individuals at each timestep
     G = func_uniqueS(y, nstatus, InfectSize, timestep1) #Make sure the infectees by nodes_names from y are susceptibles
     sbm = func_S2E(G, timestep1) #Spread the disease within the network (from susceptible to exposed)
     nstatus = func_E2I(sbm, G, timestep1) #Allow the disease to progress to infectious and recovered statuses
 
+    global G, sbm, nstatus
+
     #Count the incidence of SEIR
     for timestep2 in 1:(round(Int,endtime))
-        D = func_countelements(nstatus[:,timestep2+1])
+
+        D = func_countelements(nstatus[:,timestep2])
         D = insertcols!(D, 1, S=0, makeunique=true) #Set S as zero if column does not exist
         D = insertcols!(D, 1, E=0, makeunique=true) #Set E as zero if column does not exist
         D = insertcols!(D, 1, I=0, makeunique=true) #Set I as zero if column does not exist
         D = insertcols!(D, 1, R=0, makeunique=true) #Set R as zero if column does not exist
-        D_s = D[1,:S] #Extract the S value from D
-        D_e = D[1,:E] #Extract the E value from D
-        D_i = D[1,:I] #Extract the E value from D
-        D_r = D[1,:R] #Extract the R value from D
+
+        #Place initial number of cases onto sbm_sol for the record
+        if timestep2 == 1
+            D[1,:S] = N - casenum0
+            D[1,:E] = 0
+            D[1,:I] = casenum0
+            D[1,:R] = 0
+        end
 
         #Put these SEIR incidence values into a DataFrame sbm_sol
-        sbm_sol[timestep2+1,:S] = D_s #Put S value into the appropriate cell
-        sbm_sol[timestep2+1,:E] = D_e #Put E value into the appropriate cell
-        sbm_sol[timestep2+1,:I] = D_i #Put I value into the appropriate cell
-        sbm_sol[timestep2+1,:R] = D_r #Put R value into the appropriate cell
-        sbm_sol[timestep2+1,:N] = sbm_sol[timestep2+1,:S] + sbm_sol[timestep2+1,:E] + sbm_sol[timestep2+1,:I] + sbm_sol[timestep2+1,:R] #The total number of the population, for accurancy check
+        sbm_sol[timestep2,:S] = D[1,:S] #Put S value into the appropriate cell
+        sbm_sol[timestep2,:E] = D[1,:E] #Put E value into the appropriate cell
+        sbm_sol[timestep2,:I] = D[1,:I] #Put I value into the appropriate cell
+        sbm_sol[timestep2,:R] = D[1,:R] #Put R value into the appropriate cell
+        sbm_sol[timestep2,:N] = sbm_sol[timestep2,:S] + sbm_sol[timestep2,:E] + sbm_sol[timestep2,:I] + sbm_sol[timestep2,:R] #The total number of the population, for accurancy check
+
+        global sbm_sol
     end
-    sbm_soln = sbm_sol[sbm_sol[:N].!=0,:] #Remove the 1st row, where N=0
-    global G, sbm, nstatus, sbm_soln
 end
-#println(first(nstatus[11:20],10))
-#println(last(sbm_soln,10))
+#println(first(sbm_sol,10))
 
 # Write key results into files
 CSV.write("./data/sbm_nodes.csv", sbm, writeheader=true)
-CSV.write("./data/sbm_sol.csv", sbm_soln, writeheader=true)
+CSV.write("./data/sbm_sol.csv", sbm_sol, writeheader=true)
 
 #Plot graphs
-plot([sbm_soln.S, sbm_soln.E, sbm_soln.I, sbm_soln.R], label=["S","E","I","R"], title="Stochastic Block Model of the Population", xlabel="Time (days)", ylabel="Number of Population")
+plot([sbm_sol.S, sbm_sol.E, sbm_sol.I, sbm_sol.R], label=["S","E","I","R"], title="Stochastic Block Model of the Population", xlabel="Time (days)", ylabel="Number of Population")
 
 print("Processing time:")
 end #stop timeing the processing time
