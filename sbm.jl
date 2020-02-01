a52f4f17bb632e2afecd7ac170fad8e49d5fba01
Kendra Wu <40707182+kendrawu@users.noreply.github.com>
2020-02-01 21:54:06 +0000

    Update sbm.jl

--- sbm.jl
+++ sbm.jl
@@ -1,5 +1,5 @@
 # Stochastic block network model
-# Author: Kendra Wu
+# Authors: Kendra Wu, Ben Cooper 
 # Date: 20 January 2020
 
 # Simulates disease transmission of a 3-level clustered network with contact structure and imported cases at random time and clusters based on stochastic block model (SBM) using Ebola-like parameters from Hitchings et al (2018).
@@ -626,45 +626,11 @@ end
 sbm_sol = sbm_sol[1:size(sbm_sol,1) .!= 1,: ] # Delete the dummy row 1 from sbm_sol
 #println(first(sbm_sol,10))
 
-
-
 # Check the algorithm
 # Compute R0 in a network
 k = sum(sum(Gc))/N # mean degree of the network
 T = mean(T_arr[T_arr .> 0]) # Average probability that an infectious individual will transmit the disease to a susceptible individual with whom they have contact
 R0 = T * (k^2/k - 1)
-#println("k = ", k, ", T = ",T, ", and R0 = ",R0)
-
-# Final size
-#CI = zeros(size(communitysize_arr,1))
-#finalsize_cluster = zeros(size(communitysize_arr,1))
-#if R0>1
-#    for i in 1:size(communitysize_arr,1)
-#        CI[i] = 1/(1-R0)/communitysize_arr[i]
-#        finalsize_cluster[i] = 1 - exp(-R0 * CI[i])
-#        finalsize = sum(finalsize_cluster)
-#    end
-#else
-#    finalsize = N-1/(1-R0) # for large population
-#end
-#println(finalsize)
-
-#y = zeros(N)
-#s_infinity = zeros(N)
-# When R0<1
-#if R0>1
-#    finalsize = exp(0)
-#else
-#    for i in 1:N
-#        s_infinity[i] = i
-#        y[i] = log(S0) - log(s_infinity[i]) - R0*(1-s_infinity[i]/N)
-#        if y[i] == 0
-#            finalsize = N-round.(s_infinity[i])
-#        end
-#    end
-#end
-#println(finalsize)
-#plot(y, 1:N, legend=false)
 
 print("Processing time:")
 end #stop timeing the processing time
