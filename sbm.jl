f7ec9fb70d7f9c8a35a71bb480fb4a0164f9a56a
Kendra Wu <40707182+kendrawu@users.noreply.github.com>
2020-02-05 04:46:46 +0000

    Speed up Julia
    Comment out computations no longer needed

--- sbm.jl
+++ sbm.jl
@@ -203,8 +203,8 @@ function fn_importcases(par_disease, importcasenum_timeseries, nstatus, timestep
     for index1 in 1:(size(importcases,1))
 
         # Compute the parameters for disease properties
-        incubperiod_avg = ceil(par_disease[1,:incubperiod_shape]/par_disease[1,:incubperiod_rate])
-        infectperiod_avg = ceil(par_disease[1,:infectperiod_shape]/par_disease[1,:infectperiod_rate])
+        #incubperiod_avg = ceil(par_disease[1,:incubperiod_shape]/par_disease[1,:incubperiod_rate])
+        #infectperiod_avg = ceil(par_disease[1,:infectperiod_shape]/par_disease[1,:infectperiod_rate])
         incubperiod = rand(Gamma(par_disease[1,:incubperiod_shape],1/par_disease[1,:incubperiod_rate]),1) # Incubation period of the disease
         infectperiod = rand(Gamma(par_disease[1,:infectperiod_shape],1/par_disease[1,:infectperiod_rate]),1) # Infectious period of the disease
 
